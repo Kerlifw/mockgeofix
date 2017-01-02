6ec98223b25f956e7ee2f49bd1fe07ebe845151e
Kerlifw <kerlifw@gmail.com>
2017-01-02 20:49:27 +0800

    add leaflet-routing-machine

--- helper_scripts/whereami/map.js
+++ helper_scripts/whereami/map.js
@@ -1,5 +1,5 @@
 function init_map() {
-    var map = L.map('map').setView([51.505, -0.09], 16);
+    var map = L.map('map').setView([35.68466, 139.73523], 16);
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -23,6 +23,42 @@ function init_map() {
     }
 
     setInterval(update, 500);
+
+    //-------
+    let setter = (function *() {
+        let control = L.Routing.control({
+        }).addTo(map);
+
+        control.on('routesfound', function(e) {
+            console.log(e.routes);
+            let route = e.routes[0];
+            //for (let v of route.coordinates) {
+                //L.marker([v.lat, v.lng]).addTo(map);
+            //}
+            $.ajax({
+                method: "PUT",
+                url: "/path",
+                data: JSON.stringify(route.coordinates)
+            }).done(function() {
+            });
+        });
+
+        let waypoints = [];
+        while(true) {
+            waypoints.push(yield);
+            control.spliceWaypoints(0, 2);
+
+            waypoints.push(yield);
+            control.setWaypoints(waypoints);
+            waypoints = [];
+        }
+    })();
+
+    setter.next();
+    map.on('click', function (e) {
+        //console.log(e);
+        setter.next(e.latlng);
+    });
 }
 
 $(document).ready(init_map)
